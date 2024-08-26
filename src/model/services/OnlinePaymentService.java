2a1404b144bd24b253120da9e410424fcb6e3007
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-26 10:59:26 -0300

    services

--- src/model/services/OnlinePaymentService.java
+++ src/model/services/OnlinePaymentService.java
@@ -2,4 +2,7 @@ package model.services;
 
 public interface OnlinePaymentService {
 
+	double paymentFee(Double amount);
+	double interest(Double amount, Integer months);
+	
 }
--- src/model/services/PaypalService.java
+++ src/model/services/PaypalService.java
@@ -1,5 +1,17 @@
 package model.services;
 
-public class PaypalService {
+public class PaypalService implements OnlinePaymentService {
 
+	@Override
+	public double paymentFee(Double amount) {
+		return 0;
+	}
+
+	
+	
+	@Override
+	public double interest(Double amount, Integer months) {
+		return 0;
+	}
+	
 }
